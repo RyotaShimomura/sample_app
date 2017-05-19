0526e39dcc9a2ce6968aef7383c036d751372650
Ryota Shimomura <s1713111@s.tsukuba.ac.jp>
2017-05-19 15:36:29 +0900

    Gemgileの更新

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
   
   #ページごとの完全なタイトルを返します
-  def full_title(page_title = '')
+  def full_title(page_title = '')        #メソッド定義とオプション引数
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
       base_title
     else
-      page_title + " | " + base_title
+      page_title + " | " + base_title   #文字列の結合
     end
   end
 end
